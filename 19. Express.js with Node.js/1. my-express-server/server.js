c1c67fe4d86f1d4aa275c191ccb949e50d246b74
Faran Mohammad <ffaranm15@gmail.com>
2020-08-08 19:33:14 +0530

    completed Nodemon

--- 19. Express.js with Node.js/1. my-express-server/server.js
+++ 19. Express.js with Node.js/1. my-express-server/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(request, response) {
-    response.send("Hello!");
+app.get("/", function(req, res) {
+    res.send("<h1>Hello!</h1>");
+})
+
+app.get("/contact", function(req, res) {
+    res.send('Feel free to contact me at <a href="mailto: ffaranm15 @gmail.com ">ffaranm15@gmail.com</a>!');
+})
+
+app.get("/about", function(req, res) {
+    res.send("I am a web developer and a designer!");
+})
+
+app.get("/hobby", function(req, res) {
+    res.send("<ul><li>Coding</li><li>Technical Blogs</li><li>Netflix</li></ul>");
 })
 
 app.listen(3000, function() {
