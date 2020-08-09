96408ca72ec1a2a17274dff0150ac9cac7e092de
Faran Mohammad <ffaranm15@gmail.com>
2020-08-09 20:09:39 +0530

    completed Responding to Requests with HTML Files

--- 19. Express.js with Node.js/2. Calculator/calculator.js
+++ 19. Express.js with Node.js/2. Calculator/calculator.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello, world!");
+    res.sendFile(__dirname + "/index.html");
+    // console.log(__dirname);
 })
 
 app.listen(3000, function() {
