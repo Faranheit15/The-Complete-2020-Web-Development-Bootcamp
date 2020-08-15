1d584895cdda66ba56d51bbadfd3c3eb5295784f
Faran Mohammad <ffaranm15@gmail.com>
2020-08-15 18:34:05 +0530

    started MailChimp API

--- 20. API/2. Newsletter-SignUp/app.js
+++ 20. API/2. Newsletter-SignUp/app.js
@@ -4,7 +4,21 @@ const request = require("request");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log(firstName, lastName, email);
+})
 
 app.listen(PORT, function() {
     console.log("Server started at port 3000!");
