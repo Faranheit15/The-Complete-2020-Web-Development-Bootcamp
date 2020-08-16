dcfb583f267b4fa5f4c72b2c3f4be7b32027353b
Faran Mohammad <ffaranm15@gmail.com>
2020-08-16 19:38:49 +0530

    completed MailChimp newsletter

--- 20. API/2. Newsletter-SignUp/app.js
+++ 20. API/2. Newsletter-SignUp/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const { response } = require("express");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -14,10 +16,46 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    console.log(firstName, lastName, email);
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/8d2a8c2f1e";
+
+    const options = {
+        method: "POST",
+        auth: "faranheit:388ac0b5d9dfd6f5654f23225896691e-us17"
+    }
+
+    const request = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
 })
 
 app.listen(PORT, function() {
