1c26150c31b38c35b73c2ae2158b32062b1e7876
Faran Mohammad <ffaranm15@gmail.com>
2020-08-19 14:57:28 +0530

    started EJS Templates

--- 22. EJS/1. ToDo List v1/app.js
+++ 22. EJS/1. ToDo List v1/app.js
@@ -6,7 +6,13 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    var today = new Date();
+
+    if ((today.getDay() === 6) || (today.getDay() === 0)) {
+        res.send("<h1>Yaay! It's the weekend!</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html");
+    }
 })
 
 app.listen(PORT, function() {
