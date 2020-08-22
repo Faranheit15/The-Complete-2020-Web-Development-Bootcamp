983b5055b5249f07a299eddc5116755b47c24093
Faran Mohammad <ffaranm15@gmail.com>
2020-08-22 12:00:44 +0530

    completed ToDo List

--- 22. EJS/1. ToDo List v1/app.js
+++ 22. EJS/1. ToDo List v1/app.js
@@ -3,28 +3,45 @@ const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
+var items = ["Buy food", "Cook food", "Eat food"];
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    if ((currentDay === 6) || (currentDay === 0)) {
-        day = "Weekend";
-    } else if (currentDay === 1) {
-        day = "Monday";
-    } else if (currentDay === 2) {
-        day = "Tuesday";
-    } else if (currentDay === 3) {
-        day = "Wednesday";
-    } else if (currentDay === 4) {
-        day = "Thursday";
-    } else if (currentDay === 5) {
-        day = "Friday";
-    }
-    res.render("list", { kindOfDay: day });
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+    // var currentDay = today.getDay();
+    // var day = "";
+    // if ((currentDay === 6) || (currentDay === 0)) {
+    //     day = "Weekend";
+    // } else if (currentDay === 1) {
+    //     day = "Monday";
+    // } else if (currentDay === 2) {
+    //     day = "Tuesday";
+    // } else if (currentDay === 3) {
+    //     day = "Wednesday";
+    // } else if (currentDay === 4) {
+    //     day = "Thursday";
+    // } else if (currentDay === 5) {
+    //     day = "Friday";
+    // }
+    res.render("list", { kindOfDay: day, newListItems: items });
+})
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 })
 
 app.listen(PORT, function() {
