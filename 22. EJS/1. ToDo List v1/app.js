99a9508f88803231b794fb1c2bc2efbec14bd7a5
Faran Mohammad <ffaranm15@gmail.com>
2020-08-22 13:21:38 +0530

    completed Section 22: EJS

--- 22. EJS/1. ToDo List v1/app.js
+++ 22. EJS/1. ToDo List v1/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const PORT = process.env.PORT || 3000;
 
 let items = [];
+let workItems = [];
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,22 +14,34 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date.getDate();
     res.render("list", { listTitle: day, newListItems: items });
 })
 
 app.post("/", function(req, res) {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+})
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/about", function(req, res) {
+    res.render("about");
 })
 
 app.listen(PORT, function() {
