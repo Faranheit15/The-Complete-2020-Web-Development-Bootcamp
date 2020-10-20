ff938c05715ede9954d5074d22f389e9b2dd5c92
Faranheit15 <ffaranm15@gmail.com>
2020-10-20 19:19:54 +0530

    refactor code

--- 32. Authentication/app.js
+++ 32. Authentication/app.js
@@ -3,10 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-// const encrypt = require('mongoose-encryption');
-// const md5 = require('md5');
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
@@ -43,13 +39,10 @@ const userSchema = new mongoose.Schema({
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+passport.use(User.createStrategy());
+
 passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
@@ -64,7 +57,7 @@ passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         callbackURL: "http://localhost:3000/auth/google/secrets",
-        userProfileURL: "https://googleapis.com/oauth2/v3/userinfo"
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refreshToken, profile, cb) {
         console.log(profile);
@@ -97,11 +90,6 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/secrets", function(req, res) {
-    // if (req.isAuthenticated()) {
-    //     res.render("secrets");
-    // } else {
-    //     res.redirect("/login");
-    // }
     User.find({ "secret": { $ne: null } }, function(err, foundUsers) {
         if (err) {
             console.log(err);
@@ -144,20 +132,6 @@ app.get("/logout", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    //     const newUser = new User({
-    //         email: req.body.username,
-    //         password: hash
-    //     });
-    //     newUser.save(function(err) {
-    //         if (err) {
-    //             console.log(err);
-    //         } else {
-    //             res.render("secrets");
-    //         }
-    //     });
-    // });
-
     User.register({ username: req.body.username }, req.body.password, function(err, user) {
         if (err) {
             console.log(err);
@@ -171,26 +145,6 @@ app.post("/register", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
-    // const username = req.body.username;
-    // const password = req.body.password;
-
-    // User.findOne({ email: username }, function(err, foundUser) {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         if (foundUser) {
-    //             // if (foundUser.password === password) {
-    //             bcrypt.compare(password, foundUser.password, function(err, result) {
-    //                 if (result === true) {
-    //                     res.render("secrets");
-    //                 }
-    //             });
-
-    //             //}
-    //         }
-    //     }
-    // });
-
     const user = new User({
         username: req.body.username,
         password: req.body.password
